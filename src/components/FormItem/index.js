3124abd0803f6a13ffd36ba9cef6e2dabb5b4d51
Stephan <stephanflores30@gmail.com>
2021-09-22 17:52:48 +0200

    Ajout du h1 infos génrérale

--- src/components/FormItem/index.js
+++ src/components/FormItem/index.js
@@ -15,7 +15,6 @@ class FormItem extends React.Component {
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this);
 
-
   }
 
   // Permet de changer le state du champs
@@ -25,33 +24,36 @@ class FormItem extends React.Component {
 
   //Permet de renvoyer les résultat apres le submit
   handleSubmit(event) {
-    console.log('Le nom est ' + this.state.nom +' Le métier est ' + this.state.select);
+    console.log('Le nom est ' + this.state.nom + ' Le métier est ' + this.state.select);
     event.preventDefault();
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <div className="form">
 
-          <div className="form__name">
-            <label htmlFor="nom">Votre nom</label>
-            <input type="text" id="nom" name="nom" onChange={this.handleChange}/>
-          </div>
+      <form className="form" onSubmit={this.handleSubmit}>
 
-          <div className="form__select">
-            <label htmlFor="select">Veuillex choisir votre metier </label>
-            <select value={this.state.select} name="select" onChange={this.handleChange}>
-              <option value="job1">Métier 1</option>
-              <option value="job2">Métier 2</option>
-              <option value="job3">Métier 3</option>
-            </select>
-          </div>
+        <h1 className="title__h1">Informations générales</h1>
 
-          <input type="submit" value="Envoyer" className="form__submit"/>
 
+        <div className="form__name">
+          <label htmlFor="nom">Votre nom</label>
+          <input type="text" id="nom" name="nom" onChange={this.handleChange}/>
         </div>
+
+        <div className="form__select">
+          <label htmlFor="select">Veuillex choisir votre metier </label>
+          <select value={this.state.select} name="select" onChange={this.handleChange}>
+            <option value="job1">Métier 1</option>
+            <option value="job2">Métier 2</option>
+            <option value="job3">Métier 3</option>
+          </select>
+        </div>
+
+        <input type="submit" value="Envoyer" className="form__submit"/>
+
       </form>
+
     );
   }
 
