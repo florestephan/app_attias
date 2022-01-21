9409d72c1037599233de31bd7e21435d44c51108
Stephan <stephanflores30@gmail.com>
2022-01-21 02:54:01 +0100

    data show on page

--- src/components/FormResults/index.js
+++ src/components/FormResults/index.js
@@ -1,50 +1,84 @@
-import React, {useState, useContext} from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 import {FirebaseContext} from '../Firebase';
-import {CSVLink, CSVDownload} from "react-csv";
-import {Container} from "@mui/material";
+
+import Header from "src/components/Header";
+import Customers from "src/components/Customers";
+import ExportCSV from "src/components/ExportCSV";
+import ExportReactCSV from "src/components/ExportReactCSV";
 
 const FormResults = () => {
-
   const firebase = useContext(FirebaseContext);
-  const [data, setData] = useState('');
+  const [data, setData] = useState({});
 
 
-  firebase.data('forms').get()
-    .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        setData(doc.data())
+  useEffect(() => {
+    firebase.data('forms').get()
+      .then((doc) => {
+        doc.forEach((doc) => {
+          const myData = doc.data();
+          setData(myData)
+        })
       });
-    });
+  }, [])
 
-  let object =
-    {
-      "Problèmes de mémoire la journée ?": "non",
-      "Maux de tête le matin ?": "non",
-      "hadD": 19,
-      "Profession": "dsfgdsgdfsgds",
-      "Etes vous irritable la journée ?": "non",
-      "Prénom": "Stéphan",
-      "DOB": "1998-12-30",
-      "Sexe": "homme",
-      "Fumeur": "non",
-      "hadA": 21,
-      "Nom de famille": "Flores",
-      "Somnolence au volant ?": "non",
-      "Ronflement": "non",
-      "epworth": 24,
-      "Nomnres d'urine par nuit ?": "4",
-      "Sommeil est réparateur ?": "non",
-      "Accident endormissement au volant ?": "non",
-      "pichot": 32,
-      "Dormez-vous seul(e)": "non",
-      "Pauses respiratoires la nuit ?": "non"
+  const headers = [
+    {label: "Prénom", key: "firstName"},
+    {label: "Nom de famille", key: "lastName"},
+    {label: "Date de naissance", key: "dob"},
+    {label: "Sexe", key: "sexe"},
+    {label: "Fumeur", key: "fumeur"}
+  ];
+
+
+  function customersData() {
+    const custs = [];
+    for (let i = 0; i <= 2; i++) {
+      custs[i] = {
+        firstName: `${data.Prénom}`,
+        lastName: `${data.nom_de_famille}`,
+        email: `mail${i}@mail.com`,
+        address: `#${i}, block name, floor #${i} street name, city name, state name`,
+        postcode: `${i}0000`
+      };
     }
+    return custs;
+  }
 
+  let customers = customersData();
+
+  const wscols = [
+    {wch: Math.max(...customers.map(customer => customer.firstName.length))},
+    {wch: Math.max(...customers.map(customer => customer.lastName.length))},
+    {wch: Math.max(...customers.map(customer => customer.email.length))},
+    {wch: Math.max(...customers.map(customer => customer.address.length))},
+    {
+      wch: Math.max(...customers.map(customer => customer.postcode.length)) + 3
+    }
+  ];
 
   return (
-    <Container>
-    </Container>
-  )
+    <div className="App">
+      <Header className="header" topicTitle="Informations patient"/>
+      <div className="row">
+        <div className="col-md-8">
+        </div>
+        <div className="col-md-4 center">
+          <ExportCSV
+            csvData={customers}
+            fileName="Customers_Infomation_xlsx"
+            wscols={wscols}
+          />
+          <ExportReactCSV
+            csvHeaders={headers}
+            csvData={customersData()}
+            fileName="Customers_Infomations_csv.csv"
+          />
+        </div>
+      </div>
+      <Customers customers={customers}/>
+    </div>
+  );
 };
+
 
 export default FormResults;
